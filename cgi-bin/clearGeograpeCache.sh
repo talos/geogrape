9c5e643cb5f1671ba34a0e81c6eec3145c03b759
John Krauss <irving.krauss@gmail.com>
2012-02-24 00:13:25 -0500

    sites->site

--- cgi-bin/clearGeograpeCache.sh
+++ cgi-bin/clearGeograpeCache.sh
@@ -3,14 +3,14 @@ rm -f pieCharts/tmp/*
 
 mkdir -p overlays/tmp/city
 mkdir -p graphs/tmp
-mkdir -p ../sites/overlays/tmp/city
+mkdir -p ../site/overlays/tmp/city
 rm -f overlays/tmp/city/*
 rm -f graphs/tmp/city/*
-rm -f ../sites/overlays/tmp/city/*
+rm -f ../site/overlays/tmp/city/*
 
 mkdir -p overlays/tmp/tract
 mkdir -p graphs/tmp
-mkdir -p ../sites/overlays/tmp/tract
+mkdir -p ../site/overlays/tmp/tract
 rm -f overlays/tmp/tract/*
 rm -f graphs/tmp/tract/*
 rm -f ../site/overlays/tmp/tract/*
