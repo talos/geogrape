b4cb73892c23c118b8271af4d1ba5be36f9927d7
talos <irving.krauss@gmail.com>
2012-02-24 00:03:27 -0500

    adding clearGeograpeCache

---
+++ cgi-bin/clearGeograpeCache.sh
@@ -0,0 +1,16 @@
+mkdir -i pieCharts/tmp
+rm -f pieCharts/tmp/*
+
+mkdir -i overlays/tmp/city
+mkdir -i graphs/tmp
+mkdir -i ../sites/overlays/tmp/city
+rm -f overlays/tmp/city/*
+rm -f graphs/tmp/city/*
+rm -f ../sites/overlays/tmp/city/*
+
+mkdir -i overlays/tmp/tract
+mkdir -i graphs/tmp
+mkdir -i ../sites/overlays/tmp/tract
+rm -f overlays/tmp/tract/*
+rm -f graphs/tmp/tract/*
+rm -f ../sites/overlays/tmp/tract/*
